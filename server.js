c3cada9e995e884bd9faa25adfe8814add7735c0
Beto Lopez <363alberto@gmail.com>
2023-01-08 10:12:47 -0400

    added mysql.createConnection function

--- server.js
+++ server.js
@@ -10,6 +10,18 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // MySQL username,
+      user: 'root',
+      // TODO: Add MySQL password here
+      password: '',
+      database: 'employees_db'
+    },
+    console.log(`Connected to the employee database.`)
+  );
+
 inquirer.prompt([
         {
             type: 'list',
