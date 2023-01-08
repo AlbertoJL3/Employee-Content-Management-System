08b42d4af89baa6531e0366c18911c702403780b
Beto Lopez <363alberto@gmail.com>
2023-01-08 19:25:29 -0400

    added addEmployee function

--- db/schema.sql
+++ db/schema.sql
@@ -17,7 +17,7 @@ CREATE TABLE role (
 );
 
 CREATE TABLE employee (
-  id INT PRIMARY KEY,
+  id INT PRIMARY KEY AUTO_INCREMENT,
   first_name VARCHAR(30),
   last_name VARCHAR(30),
   role_id INT,
--- server.js
+++ server.js
@@ -226,29 +226,91 @@ async function addRole(connection) {
     }
 }
 
+// Add an employee
+async function addEmployee(connection) {
+    try {
+        // Retrieve the list of roles and employees from the database
+        const roleSql = 'SELECT id, title FROM role';
+        const employeeSql = 'SELECT id, first_name, last_name FROM employee';
+        connection.query(roleSql, (err, roleRows) => {
+            if (err) {
+                console.log(err)
+                return;
+            }
+            connection.query(employeeSql, (err, employeeRows) => {
+                if (err) {
+                    console.log(err)
+                    return;
+                }
+
+                // Extract the role titles and employee names from the rows
+                const roles = roleRows.map(row => ({
+                    name: row.title,
+                    value: row.id,
+                }));
+                const employees = employeeRows.map(row => ({
+                    name: `${row.first_name} ${row.last_name}`,
+                    value: row.id,
+                }));
+
+                // Prompt the user to enter the employee details
+                inquirer.prompt([
+                    {
+                        type: 'input',
+                        name: 'firstName',
+                        message: 'Enter the first name of the employee:',
+                    },
+                    {
+                        type: 'input',
+                        name: 'lastName',
+                        message: 'Enter the last name of the employee:',
+                    },
+                    {
+                        type: 'list',
+                        name: 'role',
+                        message: 'Select the role for the employee:',
+                        choices: roles,
+                    },
+                    {
+                        type: 'list',
+                        name: 'manager',
+                        message: 'Select the manager for the employee:',
+                        choices: ['None'].concat(employees),
+                    },
+                ]).then(answer => {
+                    // Add the employee to the database
+                    const params = []
+                    const firstName = answer.firstName
+                    const lastName = answer.lastName
+                    const role = answer.role
+                    let manager = null
+                    if (answer.manager !== 'None') {
+                        manager = answer.manager
+                    }
+                    const sql = 'INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)';
+                    connection.query(sql, [firstName, lastName, role, manager], (err, result) => {
+                        if (err) {
+                            console.log(err)
+                            return;
+                        }
+                        console.log(
+                            `Added employee: ${firstName} ${lastName} (Role ID: ${role}, Manager ID: ${manager})`
+                        );
+                        main();
+                    });
+                });
+            });
+        });
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+
+
 // Start the application
 main();
-// GIVEN a command-line application that accepts user input
-// WHEN I start the application
-// THEN I am presented with the following options: view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
 
-//View All Departments - Simple Inquirer prompt that allows user to pick:
-//View ALL Departments
-// WHEN I choose to view all departments
-// THEN I am presented with a formatted table showing department names and department ids
-//View ALL roles
-// WHEN I choose to view all roles
-// THEN I am presented with the job title, role id, the department that role belongs to, and the salary for that role
-//View ALL employess
-// WHEN I choose to view all employees
-// THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
-//Add a department
-// WHEN I choose to add a department
-// THEN I am prompted to enter the name of the department and that department is added to the database
-//Add a role
-// WHEN I choose to add a role
-// THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
-//Add an employee
 // WHEN I choose to add an employee
 // THEN I am prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
 //update an employee role
